3a7291c83bc4b5d3dc429ab345eaaf8112162f5d
knanfuri <knanfuri@yahoo.com>
2019-05-03 13:21:50 -0500

    update

--- models/members.js
+++ models/members.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
-// import mongoose from 'moongoose';
+
 const Schema = mongoose.Schema;
 
 const members = new Schema({
   module: { type: String, required: true },
-  module_score: { type: String, required: true },
+  score: { type: Number, required: true },
   Question: String,
-  Answer:Boolean,
-  age:{type:Number, min:5},
   date: { type: Date, default: Date.now }
 });
 
