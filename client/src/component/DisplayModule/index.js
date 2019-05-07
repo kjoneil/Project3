bdc22db0ea9c3771505509bb8d5acb3609465dd1
Alex Elkhatib <alexanderelkhatib@yahoo.com>
2019-05-07 14:40:58 -0500

    yes

---
+++ client/src/component/DisplayModule/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './title.css';
+function DisplayModule() {
+    return (
+        <container className="ModuleOne">
+            <row>
+                <col></col>
+                <col></col>
+            </row>
+        </container>
+    );
+}
+
+export default DisplayModule;
