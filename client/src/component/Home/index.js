6a44cf7d8164aee6fc8e82fe90d97f27c6752743
kjoneil <kjoneil.rw@gmail.com>
2019-05-07 12:55:48 -0500

    update

--- client/src/component/Home/index.js
+++ client/src/component/Home/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 import { withAuthorization } from '../Session';
 
 const HomePage = () => (
   <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+    <div>
+    <Card>
+        <CardImg top width="100%" src="https://t1.ftcdn.net/jpg/01/90/64/10/400_F_190641028_3JgI4YpJOQbZa49SEi88y55W4axQfgbY.jpg" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>  
   </div>
 );
 
